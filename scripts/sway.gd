d63cec977fb9773f05e58abb94df6a14809d774a
Mike / iznogoud / fierc3 <mpullen9@gmail.com>
2024-06-02 15:34:33 +0200

    Add slight camera sway pon moving

--- scripts/sway.gd
+++ scripts/sway.gd
@@ -9,6 +9,7 @@ extends Node3D
 # Original positions
 var original_arms_position: Vector3
 var original_gun_position: Vector3
+var original_camera_position: Vector3
 
 # Time variable for swinging
 var time: float = 0.0
@@ -24,12 +25,12 @@ func _ready():
 	# Get references to the arms and gun nodes
 	original_arms_position = $"../arms_root".global_transform.origin
 	original_gun_position = $"../gunrig".global_transform.origin
+
 	previous_position = $".".global_position
 	arms = $"../arms_root"
 	gun = $"../gunrig"
 
-func _physics_process(delta):
-	
+func _physics_process(delta):	
 	# Update time
 	time += delta
 	
@@ -42,10 +43,16 @@ func _physics_process(delta):
 		swing_speed = 3 # tune this for swing speed
 	else:
 		swing_speed = 8 # tune this for swing speed
-	
 	# Calculate the swinging motion				tune this for spread
 	var swing = sin(time * swing_speed) * (velocity_summary/200)
+	
+	var camera_swing = 0 if velocity_summary <= 1.1 else swing * 10
 
 	# Apply the swinging motion
 	arms.transform.origin.x = original_arms_position.x + swing
 	gun.transform.origin.x = original_gun_position.x + swing - correction
+
+	# Add swing to camera, should be minimal due to the fact we add spread / inaccuracy via raycast spread
+	var final_camera_swing = (camera_swing * camera_swing) * 0.2
+	Global.WEAPON_CAMERA.global_transform.origin.y += final_camera_swing * 1.2
+	Global.WEAPON_CAMERA.global_transform.origin.x += final_camera_swing 
